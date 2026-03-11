ed22fdb43ee8fdedc4fe9fca0753f5db827708d4
prajwal-creator <prajwalgoutham962@gmail.com>
2026-03-11 20:46:08 +0530

    first commit

---
+++ basic.java
@@ -0,0 +1,12 @@
+class basic{
+    public static void main(String[] args) {
+        int a=10,b=20;
+        System.out.println("hello world");
+        System.out.println("the a value is"+a+"the b value is"+b);
+        if(a>b){
+            System.out.println("a is greater b"+a);
+        }else{
+            System.out.println("b is greater "+b);
+        }
+    }
+}
