8a67e084709e2a0acca05cef5d8a7b8f3c6acd9a
prajwal-creator <prajwalgoutham962@gmail.com>
2026-03-11 21:02:23 +0530

    learnt arrays and reversed

---
+++ rev.java
@@ -0,0 +1,13 @@
+public class rev {
+    public static void main(String[] args){
+       int arr[] ={10,20,30,40,50};
+       int i;
+
+       for(i=4;i>0;i--){
+        System.out.print(arr[i]);
+       }
+
+
+    }
+    
+}
